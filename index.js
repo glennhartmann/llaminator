c3f86d9b9a72a9df8a4fe6d9f894f57c959b3b05
Glenn Hartmann <hartmann.glenn@gmail.com>
2021-12-02 21:56:34 -0500

    use 'dist/' output from 30d9978126d4f8c318128abae1a355a6d4ec2ec6

--- index.js
+++ index.js
@@ -1,104 +1,71 @@
-/*
- * Copyright 2021 Google Inc. All Rights Reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/llaminator/sw.js');
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/llaminator/sw.js");
   });
-} else {
-  // TODO display error message and fail gracefully
 }
-
-const mainDBName = 'appDB';
-const objStoreName = 'imageStore';
-const mainImageName = 'mainImage';
-
-window.addEventListener('load', () => {
-  const fileInput = document.querySelector('#input');
-  const imgElement = document.querySelector('img');
-  const shareBtn = document.querySelector('#share');
-
-  if (!window.indexedDB || !window.URL) { /* TODO: display error message */ }
-
+const mainDBName = "appDB";
+const objStoreName = "imageStore";
+const mainImageName = "mainImage";
+window.addEventListener("load", () => {
+  const fileInput = document.querySelector("#input");
+  const imgElement = document.querySelector("img");
+  const shareBtn = document.querySelector("#share");
   const dbOpenRequest = window.indexedDB.open(mainDBName);
-  dbOpenRequest.addEventListener('success', (e) => onDBOpenSuccess(e, imgElement, shareBtn));
-  dbOpenRequest.addEventListener('error', (e) => {
-    console.log('error loading db:', e);
-    // TODO: display error
+  dbOpenRequest.addEventListener("success", (e) => onDBOpenSuccess(e, imgElement, shareBtn));
+  dbOpenRequest.addEventListener("error", (e) => {
+    console.log("error loading db:", e);
   });
-  dbOpenRequest.addEventListener('upgradeneeded', (e) => {
+  dbOpenRequest.addEventListener("upgradeneeded", (e) => {
     console.log(`DB update request:`, e);
     e.target.result.createObjectStore(objStoreName);
   });
-
-  fileInput.addEventListener('change', (e) => onFileInputChange(e, dbOpenRequest, imgElement, shareBtn));
-
-  shareBtn.addEventListener('click', async () => {
-    const blob = await fetch(imgElement.src).then(r => r.blob());
+  fileInput.addEventListener("change", (e) => onFileInputChange(e, dbOpenRequest, imgElement, shareBtn));
+  shareBtn.addEventListener("click", async () => {
+    const blob = await fetch(imgElement.src).then((r) => r.blob());
     const f = fileFromBlob(blob);
-    navigator.share({files: [f]});
+    navigator.share({ files: [f] });
   });
 });
-
 function onDBOpenSuccess(e, imgElement, shareBtn) {
-  console.log('Database initialized');
-
+  console.log("Database initialized");
   const db = e.target.result;
-  const t = db.transaction(objStoreName, 'readonly').objectStore(objStoreName).get(mainImageName);
-  t.addEventListener('success', (e) => {
-    console.log('get success:', e)
-    const b = e.target.result;
-    if (!b) return;
+  const t = db.transaction(objStoreName, "readonly").objectStore(objStoreName).get(mainImageName);
+  t.addEventListener("success", (e2) => {
+    console.log("get success:", e2);
+    const b = e2.target.result;
+    if (!b)
+      return;
     imgElement.src = window.URL.createObjectURL(b);
     displayIfShareEnabled(shareBtn, b);
   });
-  t.addEventListener('error', (e) => {
-    console.log('get error:', e)
+  t.addEventListener("error", (e2) => {
+    console.log("get error:", e2);
   });
 }
-
 async function onFileInputChange(e, dbOpenRequest, imgElement, shareBtn) {
-  console.log(e.target.value);
-  if (e.target.files.length === 0) return;
-  const f = e.target.files[0]; // TODO: null-check
+  const inputElement = e.target;
+  console.log(inputElement.value);
+  if (!inputElement.files || !inputElement.files.length)
+    return;
+  const f = inputElement.files[0];
   const b = new Blob([await f.arrayBuffer()], { type: f.type });
-  // TODO: perhaps prompt before silently replacing old image, if one exists?
   imgElement.src = window.URL.createObjectURL(b);
   displayIfShareEnabled(shareBtn, b);
-  const t = dbOpenRequest.result.transaction(objStoreName, 'readwrite').objectStore(objStoreName).put(b, mainImageName);
-  // TODO: display "saving..." message/spinner?
-  t.addEventListener('success', (e) => {
-    console.log('put success:', e)
+  const t = dbOpenRequest.result.transaction(objStoreName, "readwrite").objectStore(objStoreName).put(b, mainImageName);
+  t.addEventListener("success", (e2) => {
+    console.log("put success:", e2);
   });
-  t.addEventListener('error', (e) => {
-    console.log('put error:', e)
-    // TODO: display error
+  t.addEventListener("error", (e2) => {
+    console.log("put error:", e2);
   });
-  // TODO: add option to remove from storage
 }
-
 function displayIfShareEnabled(target, blob) {
   const f = fileFromBlob(blob);
-  if (navigator.share && navigator.canShare &&
-      navigator.canShare({files: [f]})) {
-    target.style.display = 'block';
+  if ("share" in navigator && "canShare" in navigator && navigator.canShare({ files: [f] })) {
+    target.style.display = "block";
   }
 }
-
 function fileFromBlob(blob) {
-  return new File([blob], "name.png" /* TODO - name? Maybe we should be storing a File instead of a Blob? */,
-    {type: "image/png"});
+  return new File([blob], "name.png", { type: "image/png" });
 }
+//# sourceMappingURL=index.js.map
