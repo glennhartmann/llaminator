c5adbc63d84d4aa0b89337d3a38cde939a2e9b0a
Glenn Hartmann <hartmanng@google.com>
2022-02-05 11:40:18 -0500

    Add bubblewrap-generated Trusted Web Activity project

---
+++ bubblewrap/app/src/main/java/app/netlify/llaminator_dev/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.netlify.llaminator_dev.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
